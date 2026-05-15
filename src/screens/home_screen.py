7c55f34de36426742e09a45b9235839a464169e7
AnujVerma-hub <anujvermamarch@gmail.com>
2026-05-15 08:09:20 +0530

    Update home_screen.py

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from src.components.header import header_home
-from src.ui.base_layout import style_background_home, style_background_dashboard
+from src.ui.base_layout import style_background_home, style_base_layout
 def home_screen():
 
 
     header_home()
     col1, col2 = st.columns(2, gap="large")
-    style_base_layout_home()
+    style_background_home()
     style_base_layout()
 
     with col1:
