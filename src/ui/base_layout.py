3bf2519e1c2c67abb8953310b6c1cf83d2fa8b99
AnujVerma-hub <anujvermamarch@gmail.com>
2026-05-15 08:01:18 +0530

    Update base_layout.py

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-def style_base_layout_home():
+def style_background_home():
 
     st.markdown("""
         <style>
+
                 .stApp {
-                    background: #5865F2 !important;                
+                    background: #5865F2 !important;
                 }
 
                 .stApp div[data-testid="stColumn"]{
@@ -13,40 +14,46 @@ def style_base_layout_home():
                     padding:2.5rem !important;
                     border-radius: 5rem !important;
                     }
-                
-        </style>
+        </style>  
 
-                """,unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
     
 
-def style_base_layout_dashboard():
+def style_background_dashboard():
 
     st.markdown("""
         <style>
+
                 .stApp {
-                    background: #E0E3FF !important;                
+                    background: #E0E3FF !important;
                 }
 
-                
-        </style>
+        </style>  
 
-                """,unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
+    
+
+    
 
 def style_base_layout():
-
+# asdasd
     st.markdown("""
         <style>
         @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
-        /*  Hide top and bottom bar of streamlit */
-            #MainMenu, footer, header {
-                visibility: hidden;S
-            }  
 
+                
+         /* Hide Top Bar of streamlit */
+                
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
+                
             .block-container {
-            
-                padding-top:1.5rem !important;S
-            } 
+                padding-top:1.5rem !important;    
+            }
 
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
@@ -62,27 +69,31 @@ def style_base_layout():
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
             }
-
+                
             h3, h4, p {
-                font-family: 'Outfit', sans-serif;
-                }   
+                font-family: 'Outfit', sans-serif;    
+            }
+                
+
             button{
                 border-radius: 1.5rem !important;
                 background-color: #5865F2 !important;
+                color: white !important;
                 padding: 10px 20px !important;
                 border: none !important;
                 transition: transform 0.25s ease-in-out !important;
                 }
 
-            button[kind="secondary"] {
+            button[kind="secondary"]{
                 border-radius: 1.5rem !important;
                 background-color: #EB459E !important;
+                color: white !important;
                 padding: 10px 20px !important;
                 border: none !important;
                 transition: transform 0.25s ease-in-out !important;
                 }
 
-            button[kind="tertiary"] {
+            button[kind="tertiary"]{
                 border-radius: 1.5rem !important;
                 background-color: black !important;
                 color: white !important;
@@ -91,9 +102,9 @@ def style_base_layout():
                 transition: transform 0.25s ease-in-out !important;
                 }
 
-            button:hover {
-                transform: scale(1.05);
-                }
-        </style>
+            button:hover{
+                transform :scale(1.05)}
+        </style>  
 
-                """,unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
